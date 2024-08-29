95aafe55bc649de71691be5b282ef8e638767e72
Mohit Badola <94218660+mohitbadola@users.noreply.github.com>
2024-08-29 07:16:15 +0530

    Initial commit

---
+++ src/main/java/com/jason/SpringJDBCEx/SpringJdbcExApplication.java
@@ -0,0 +1,14 @@
+package com.jason.SpringJDBCEx;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringJdbcExApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringJdbcExApplication.class, args);
+		System.out.println("hello");
+	}
+
+}
---
+++ src/test/java/com/jason/SpringJDBCEx/SpringJdbcExApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jason.SpringJDBCEx;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringJdbcExApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
