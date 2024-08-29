56fff2cec5c3ec49bec219152e4c5f0e44eef05b
Mohit Badola <94218660+mohitbadola@users.noreply.github.com>
2024-08-29 07:47:55 +0530

    Create Student class

--- src/main/java/com/jason/SpringJDBCEx/SpringJdbcExApplication.java
+++ src/main/java/com/jason/SpringJDBCEx/SpringJdbcExApplication.java
@@ -1,14 +1,20 @@
 package com.jason.SpringJDBCEx;
 
+import com.jason.SpringJDBCEx.model.Student;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
 
 @SpringBootApplication
 public class SpringJdbcExApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SpringJdbcExApplication.class, args);
-		 System.out.println("hello");
+		ApplicationContext context = SpringApplication.run(SpringJdbcExApplication.class, args);
+		 Student st = context.getBean(Student.class);
+		 st.setName("Mohit");
+		 st.setMarks(50);
+		 st.setRollNo(1);
+		 System.out.println(st.toString());
 	}
 
 }
